c063ddafac99ce9b0a309c5bfee2b2cbf94555de
Peter Scott <pjscott@iastate.edu>
2011-04-16 15:34:50 -0500

    Add test case for MovingAverage class.

--- tests.py
+++ tests.py
@@ -3,13 +3,12 @@
 import stats
 import unittest
 
-
+pidigits = "3141592653589793238462643383279502884197169"
+pidigits = [float(c) for c in pidigits]
+        
 
 class TestMeanVariance(unittest.TestCase):
     def test_pidigits(self):
-        pidigits = "3141592653589793238462643383279502884197169"
-        pidigits = [float(c) for c in pidigits]
-
         mv = stats.MeanVariance()
         for x in pidigits:
             mv.add(x)
@@ -19,6 +18,23 @@ class TestMeanVariance(unittest.TestCase):
         self.assertTrue(abs(mv.sample_variance - 7.67663344) < 0.0001)
         self.assertTrue(abs(mv.population_variance - 7.49810708) < 0.0001)
 
+class TestMovingAverage(unittest.TestCase):
+    def test_pidigits(self):
+        ma = stats.MovingAverage(5)
+
+        for x in pidigits[:3]:
+            ma.add(x)
+
+        # Assert that when moving average object has received fewer
+        # values than its window size, it just computes the average of
+        # those values, and no dummy zeros.
+        self.assertEqual(ma.mean, (3+1+4) / 3.0)
+
+        for x in pidigits[3:]:
+            ma.add(x)
+
+        # Sum of last 5 digits in sequence
+        self.assertEqual(ma.mean, 6.4000000000000004)
 
 if __name__ == '__main__':
     unittest.main()
