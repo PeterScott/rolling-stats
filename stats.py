ba20cdd9ce5fd4c823f89c68d4f7083de15b0ecb
Peter Scott <pjscott@iastate.edu>
2011-04-16 14:37:09 -0500

    Add n-sample moving average.

--- stats.py
+++ stats.py
@@ -28,3 +28,40 @@ class MeanVariance:
     @property
     def population_variance(self):
         return self.M2 / self.n
+
+class MovingAverage:
+    """Compute an n-sample moving average. Before n samples are added,
+    averages all samples that have been received yet."""
+    
+    def __init__(self, n):
+        """Initialize, where n is the number of samples to average."""
+        self.n = n                # Number of samples
+        self.samples = [None] * n # Circular buffer
+        self.i = 0                # Insertion index into buffer
+        self.sum = 0.0            # Sum of values
+        self.full = False         # Has the buffer been filled?
+
+    def add(self, value):
+        # Value leaving the set
+        evicted = self.samples[self.i]
+        if evicted is not None:
+            self.sum -= evicted
+        
+        # Add new value to the circular buffer, update sum
+        self.samples[self.i] = value
+        self.i += 1
+        if self.i == self.n:
+            self.full = True
+            self.i = 0
+        self.sum += value
+
+    @property
+    def mean(self):
+        if not self.full and self.i == 0:
+            # Set is empty. FIXME: should this raise an exception?
+            return 0.0
+
+        if self.full:
+            return self.sum / self.n
+        else:
+            return self.sum / self.i
